473adbbfbc0cad207957964a143d8c453fd8b731
Toshiya Namikawa <namikawa.toshiya9@gmail.com>
2022-11-24 03:00:24 -0800

    auto update

--- z-clean/.ipynb_checkpoints/run_analytic_snr_zclean_cib-checkpoint.py
+++ z-clean/.ipynb_checkpoints/run_analytic_snr_zclean_cib-checkpoint.py
@@ -10,7 +10,8 @@ import local
 
 lmin, lmax = 1, 2048
 L = np.linspace(0,lmax,lmax+1)
-experiment = 'so'
+experiment = 's4'
+#experiment = 'so'
 
 k, pk0 = np.loadtxt(local.root + 'modelw/Pk/Pklin_new.dat',unpack=True)
 
@@ -42,29 +43,33 @@ skk = basic.bispec.cl_flat(local.cpmodel,z,dz,zs[:2],lmax,k,pk0)
 dkk = basic.bispec.cl_flat(local.cpmodel,z,dz,zs[:2],lmax,k,pk0,wdel=wdel)
 
 Lmaxs = np.arange(100,2100,100)
-oSNR  = np.zeros(len(Lmaxs))
-dSNR  = np.zeros(len(Lmaxs))
+oSNRt  = np.zeros(len(Lmaxs))
+oSNRm  = np.zeros(len(Lmaxs))
+dSNRt  = np.zeros(len(Lmaxs))
+dSNRm  = np.zeros(len(Lmaxs))
 
 for i, Lmax in enumerate(Lmaxs):
 
     # kappa noise spectrum
     nlkk = np.zeros(Lmax+1)
-    if experiment == 'advact':
-        nlkk[2:] = np.loadtxt(local.root+'nldd/advact_s6_t1_rlmax4000.dat',unpack=True)[1][:Lmax-1]
+    if experiment == 'actdr6':
+        nlkk[3:] = np.loadtxt(local.root+'nldd/ACTDR6.dat',unpack=True)[1][:Lmax-2]
     if experiment == 'so':
         nlkk[2:] = np.loadtxt(local.root+'nldd/so/kappa_deproj0_sens2_16000_lT30-3000_lP30-5000.dat',unpack=True)[7][:Lmax-1]
     if experiment == 's4':
-        nlkk[2:] = np.loadtxt(local.root+'nldd/S4_s1_t3_rlmax4000.dat',unpack=True)[1][:Lmax-1]
+        nlkk[2:] = np.loadtxt(local.root+'nldd/kappa_deproj0_sens0_16000_lT30-3000_lP30-5000.dat',unpack=True)[7][:Lmax-1]
+        #nlkk[2:] = np.loadtxt(local.root+'nldd/S4_s1_t3_rlmax4000.dat',unpack=True)[1][:Lmax-1]
 
     # observed kappa spectrum
     ockk = skk[:Lmax+1] + nlkk
     dckk = dkk[:Lmax+1] + nlkk
         
     # SNR computation
-    oSNR[i] = basic.bispec.bispeclens_snr(local.cpmodel,'RT',z,dz,zs,2,Lmax,ockk,k,pk0,btype='kkk')
-    dSNR[i] = basic.bispec.bispeclens_snr(local.cpmodel,'RT',z,dz,zs,2,Lmax,dckk,k,pk0,btype='kkk',wdel=wdel[:,:Lmax+1])
-    print('Lmax=',Lmax,',snr=',np.around(oSNR[i],decimals=4),',snr(del)=',np.around(dSNR[i],decimals=4))
+    oSNRt[i], oSNRm[i] = basic.bispec.bispeclens_snr(local.cpmodel,'RT',z,dz,zs,2,Lmax,ockk,k,pk0,btype='kkk')
+    dSNRt[i], dSNRm[i] = basic.bispec.bispeclens_snr(local.cpmodel,'RT',z,dz,zs,2,Lmax,dckk,k,pk0,btype='kkk',wdel=wdel[:,:Lmax+1])
+    print('Lmax=',Lmax,',snr=',np.around(oSNRt[i],decimals=4),',snr(del)=',np.around(dSNRt[i],decimals=4))
+    print('Lmax=',Lmax,',snr=',np.around(oSNRm[i],decimals=4),',snr(del)=',np.around(dSNRm[i],decimals=4))
 
-np.savetxt(local.root+local.cpmodel+'/snr/snr_kkk_zclean_cib_'+experiment+'_zn'+str(zn)+'.dat',np.array((Lmaxs,oSNR,dSNR)).T)
+np.savetxt(local.root+local.cpmodel+'/snr/snr_kkk_zclean_cib_'+experiment+'_zn'+str(zn)+'.dat',np.array((Lmaxs,oSNRt,dSNRt,oSNRm,dSNRm)).T)
 
 
